c1b03ea259b619d389708cc9da8515068d9aac16
Paweł Kowaluk <pensjonatus@gmail.com>
2024-03-07 07:38:45 +0100

    generate randomer

--- app/actions.tsx
+++ app/actions.tsx
@@ -1,7 +1,7 @@
 "use server";
 
 import { Message } from "@/components/Messages";
-import { paragraph } from "txtgen";
+import { words } from "@/utils/words";
 
 const templates = ["just-paragraphs", "bullets-in-the-middle"];
 
@@ -9,16 +9,49 @@ function getRandomInt(min = 1, max = 10) {
   return min + Math.floor(Math.random() * max);
 }
 
-function getParagraphs(count: number): Message["body"] {
-  return Array.from(Array(count)).map((item, idx) => (
-    <p key={idx}>{paragraph()}</p>
+function getWord(): string {
+  const wordIndex = getRandomInt(0, words.length - 1);
+  return words[wordIndex];
+}
+
+function getNumberOfThings(
+  minThings: number,
+  maxThings: number,
+  generatorFunction: () => string
+): string[] {
+  const howManyThings = getRandomInt(minThings, maxThings);
+  const things: string[] = [];
+  for (let i = 0; i < howManyThings; i++) {
+    things.push(generatorFunction());
+  }
+  return things;
+}
+
+function getSentence(): string {
+  const sentence = getNumberOfThings(1, 50, getWord).join(" ") + ".";
+  const capitalized =
+    sentence.slice(0, 1).toLocaleUpperCase() + sentence.slice(1);
+
+  return capitalized;
+}
+
+function getParagraph(): string {
+  return getNumberOfThings(1, 10, getSentence).join(" ");
+}
+
+function getParagraphs(): Message["body"] {
+  const paragraphs = getNumberOfThings(2, 10, getParagraph);
+  return paragraphs.map((paragraph, idx) => (
+    <p key={idx} className="mb-6">
+      {paragraph}
+    </p>
   ));
 }
 
 export async function generateResponse(): Promise<Message["body"]> {
   const template = templates[getRandomInt(0, 1)];
 
-  const result = getParagraphs(getRandomInt(5, 15));
+  const result = getParagraphs();
   return new Promise((resolve) => {
     resolve(result);
   });
