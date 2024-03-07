288b6f292974e0ff714a45e7b6d34715e1988722
Paweł Kowaluk <pensjonatus@gmail.com>
2024-03-07 08:14:04 +0100

    scroll new messages into view

---
+++ components/Message.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import clsx from "clsx";
+import { Message } from "./Messages";
+import { useEffect, useRef } from "react";
+
+export default function Message({ actor, body }: Message) {
+  const messageRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (messageRef && messageRef.current && actor === "you") {
+      messageRef.current.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+  }, [messageRef, actor]);
+  return (
+    <div
+      ref={messageRef}
+      className={clsx(
+        actor === "you"
+          ? "bg-white text-black self-end"
+          : "bg-blue-400 text-black",
+        "p-3 rounded-lg w-fit"
+      )}
+    >
+      <div className="text-sm text-right">{actor}</div>
+      <div>{body}</div>
+    </div>
+  );
+}
--- components/Messages.tsx
+++ components/Messages.tsx
@@ -1,4 +1,5 @@
 import clsx from "clsx";
+import Message from "./Message";
 
 export type Message = {
   actor: "you" | "bot";
@@ -13,19 +14,8 @@ export default function Messages({ messages }: MessagesProps) {
   return (
     <div className="border border-fuchsia-500 p-3 md:p-6 h-[60vh] overflow-y-auto w-full rounded-lg">
       <div className="flex flex-col justify-end scrollbar:!w-1.5 scrollbar:!h-1.5 scrollbar:bg-transparent scrollbar-track:!bg-slate-100 scrollbar-thumb:!rounded scrollbar-thumb:!bg-slate-300 scrollbar-track:!rounded dark:scrollbar-track:!bg-slate-500/[0.16] dark:scrollbar-thumb:!bg-slate-500/50 lg:supports-scrollbars:pr-2">
-        {messages.map(({ actor, body }, index) => (
-          <div
-            key={index}
-            className={clsx(
-              actor === "you"
-                ? "bg-white text-black self-end"
-                : "bg-blue-400 text-black",
-              "p-3 rounded-lg w-fit"
-            )}
-          >
-            <div className="text-sm text-right">{actor}</div>
-            <div>{body}</div>
-          </div>
+        {messages.map((message, index) => (
+          <Message key={index} {...message} />
         ))}
       </div>
     </div>
